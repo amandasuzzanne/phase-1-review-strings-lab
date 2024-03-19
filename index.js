9d8b8d37e787730707e82e7515c4b3319c1a2829
amandasuzzanne <amandasuzanne12@gmail.com>
2024-03-19 16:00:07 +0300

    complete strings lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Amanda Suzzanne';
+
+const welcomeMessage = 'Welcome to Flatbook, Amanda Suzzanne!';
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, AMANDA SUZZANNE!';
+
+var shortGreeting = 'Welcome, A!'
